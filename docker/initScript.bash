7f38bc91cef5738c1aab13751a89def078286f32
Hector <hector.david.1987@gmail.com>
2022-12-02 17:12:14 +0000

    fixing init script

--- docker/initScript.bash
+++ docker/initScript.bash
@@ -1,4 +1,4 @@
 #! /bin/bash
 
 #jupyter notebook --allow-root --no-browser --port 8888 --ip=0.0.0.0 --NotebookApp.token=''
-voila --template vuetify-default --enable_nbextensions=True WholeInterface.ipynb --Voila.ip=0.0.0.0 --port 8866 --no-browser
+voila --template vuetify-default --enable_nbextensions=True frontend/WholeInterface.ipynb --Voila.ip=0.0.0.0 --port 8888 --no-browser
