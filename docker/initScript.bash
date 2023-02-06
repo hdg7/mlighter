f3b4053eec91245b2e19ea159bfba52280e21dde
Hector <hector.david.1987@gmail.com>
2023-02-06 13:16:55 +0000

    fixing JavaScript incompatibility

--- docker/initScript.bash
+++ docker/initScript.bash
@@ -1,4 +1,4 @@
 #! /bin/bash
 
 #jupyter notebook --allow-root --no-browser --port 8888 --ip=0.0.0.0 --NotebookApp.token=''
-voila --template vuetify-default --enable_nbextensions=True frontend/WholeInterface.ipynb --Voila.ip=0.0.0.0 --port 8888 --no-browser
+voila --no-browser --template vuetify-default --enable_nbextensions=True frontend/WholeInterfaceUpdated.ipynb --Voila.ip=0.0.0.0 --port 8888
