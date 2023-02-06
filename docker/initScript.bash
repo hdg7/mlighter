1e21a54b92756b90de179d459ef3ffd1f631938b
Hector Menendez <hector.david.1987@gmail.com>
2023-02-06 19:06:44 +0000

    updating dockerfile and script

--- docker/initScript.bash
+++ docker/initScript.bash
@@ -1,4 +1,3 @@
-#! /bin/bash
-
+#!/bin/bash
 #jupyter notebook --allow-root --no-browser --port 8888 --ip=0.0.0.0 --NotebookApp.token=''
 voila --no-browser --template vuetify-default --enable_nbextensions=True frontend/WholeInterfaceUpdated.ipynb --Voila.ip=0.0.0.0 --port 8888
